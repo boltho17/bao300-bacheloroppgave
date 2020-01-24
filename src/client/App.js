529efb2ce89e9a8d6e7f914bc37174a9dbab8c0a
boltho17 <31183462+boltho17@users.noreply.github.com>
2020-01-24 15:43:47 +0100

    added dependencies, template-pages, sass

---
+++ src/client/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter as Router} from "react-router-dom";
+import './styles/Main.sass'
+import LandingPage from "./pages/LandingPage";
+import TopNavbar from './components/TopNavbar'
+
+const App = () => {
+    return (
+        <Router>
+            <TopNavbar/>
+            <LandingPage/>
+        </Router>
+    );
+};
+
+export default App;
---
+++ src/client/pages/LandingPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Her kommer SocialCoffee!
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Awesome
+                    </a>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default LandingPage
