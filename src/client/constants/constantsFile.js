fd9f5416c2bc997c50cd54efbcbe750ee2631aa9
boltho17 <31183462+boltho17@users.noreply.github.com>
2020-01-31 21:24:53 +0100

    implement Firebase sign up and sign in pages

--- src/client/constants/constantsFile.js
+++ src/client/constants/constantsFile.js
@@ -2,16 +2,3 @@ export const ACTION_INVALID = "This action is invalid!"
 export const CONSTANT_NUMBER_1 = 'hello I am a constant';
 export const CONSTANT_NUMBER_2 = 'hello I am also a constant';
 
-
-// FIREBASE
-export const API_KEY = "AIzaSyBC5Va5kxIBJSDO2G6XNKxhnP38_VJ_yeI";
-
-var firebaseConfig = {
-    apiKey: "AIzaSyBC5Va5kxIBJSDO2G6XNKxhnP38_VJ_yeI",
-    authDomain: "social-coffee.firebaseapp.com",
-    databaseURL: "https://social-coffee.firebaseio.com",
-    projectId: "social-coffee",
-    storageBucket: "social-coffee.appspot.com",
-    messagingSenderId: "419956585048",
-    appId: "1:419956585048:web:ef289b73271f945728502d"
-};
