03a7d96d4989bfd695155c3dca919bad85f873b0
Tobias Nikolai Mars <tobiasnikolaimars@Tobiass-MacBook-Pro.local>
2020-02-10 16:33:31 +0100

    fix

--- src/client/App.js
+++ src/client/App.js
@@ -6,11 +6,6 @@ import {
 
 import * as ROUTES from './constants/routes';
 import './styles/Main.sass'
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import LandingPage from "./pages/LandingPage";
-import TopNavbar from './components/TopNavbar'
-=======
 
 import Navigation from './components/Navigation';
 import LandingPage from "./pages/LandingPage";
@@ -18,35 +13,17 @@ import SignUpPage from './pages/SignUpPage';
 import SignInPage from './pages/SignInPage';
 import DeliveryCost from "./components/BringAPI/DeliveryCost";
 import Footer from './components/Footer';
-=======
-
-import Navigation from './components/Navigation'
-import LandingPage from "./pages/LandingPage";
-import SignUpPage from './pages/SignUpPage';
-import SignInPage from './pages/SignInPage';
-import DeliveryCost from "./components/BringAPI/DeliveryCost";
->>>>>>> origin
 
 /* TODO Pages to implement:
 import PasswordForgetPage from '../PasswordForget';
 import AccountPage from '../Account';
 import AdminPage from '../Admin';
  */
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> origin
+
 
 const App = () => {
     return (
         <Router>
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-            <TopNavbar />
-            <LandingPage />
-=======
-=======
->>>>>>> origin
             <div>
                 <Navigation />
 
@@ -55,12 +32,10 @@ const App = () => {
                 <Route path={ROUTES.SIGN_IN} component={SignInPage} />
                 <Route path="/deliverycost" component={DeliveryCost} />
             </div>
-<<<<<<< HEAD
+
             
             <Footer />
->>>>>>> Stashed changes
-=======
->>>>>>> origin
+
         </Router>
     );
 };
