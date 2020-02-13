f6b368e16cb880226b6414b54cc18977e4f3af99
Tobias Nikolai Mars <tobiasnikolaimars@Tobiass-MacBook-Pro.local>
2020-02-13 13:09:33 +0100

    Trygg punkt, la til RecProducts

--- src/client/components/RecommendedProducts.jsx
+++ src/client/components/RecommendedProducts.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Card, Button} from 'react-bootstrap';
 
 export default class RecommendedProducts extends React.Component {
     render () {
         return (
             <div className="rp-container">
                 <h2>Anbefalte Produkter</h2>
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src='http://tuscanyeasytravel.com/wp-content/uploads/2018/02/Coffee-Italiano-.jpg' />
+                <Card.Body>
+                <Card.Title>Italiano Kaffe</Card.Title>
+                <Card.Text>
+                    Fersk ny kaffe fra de praktiske fjellene i Italia, brakt til
+                    deg fra de lokale gjeitene!
+            
+                </Card.Text>
+                <Button variant="primary">Gå til produktet</Button>
+                </Card.Body>
+            </Card>
             </div>
         )
     }
