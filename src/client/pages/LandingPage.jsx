f432c2413dcacaf775d4b12654a18532d9260f77
Tobias Nikolai Mars <tobiasnikolaimars@Tobiass-MacBook-Pro.local>
2020-02-24 14:30:28 +0100

    New Banner Bar Changes

--- src/client/pages/LandingPage.jsx
+++ src/client/pages/LandingPage.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import SearchBar from '../components/SearchBar';
+
 import RecommendedProducts from '../components/RecommendedProducts';
- 
+
 
 class LandingPage extends React.Component {
 
     render() {
         return ( 
-            <div className="App">
-                
-                <header className="App-header">
-                    
-                </header>
+            <div className="">
 
                 <div className="image-container" >
-                    <SearchBar />
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-4">"God kaffe er ikke bare produsert av de Norske gjeitene fra fjellene i kypros."</div>
+                        </div>
+                        <div className="row rowBottomHeight">
+                            <div className="col-7 col-md-8"></div>
+                            <div className="col-5 col-md-4">"Beste kaffe! Produsert av de Norske gjeitene fra fjellene i kypros."</div>
+                        </div>
+                    </div>
                 </div>
                 <RecommendedProducts />
             </div>
