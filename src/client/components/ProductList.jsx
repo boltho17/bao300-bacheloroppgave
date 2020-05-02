4f5d5b0df93ac1fa56cd1f9bc63e6ee6edd2c8d3
Tobben00 <tobias.nikolai.mars@gmail.com>
2020-05-02 19:48:13 +0200

    fix merge conflict

--- src/client/components/ProductList.jsx
+++ src/client/components/ProductList.jsx
@@ -1,44 +1,14 @@
-import ProductCard from "../components/ProductCard";
-<<<<<<< HEAD
-import {useQuery} from '@apollo/react-hooks';
 import React from 'react';
-import {GET_PRODUCTS} from "../components/GraphQL/product/queries";
-=======
->>>>>>> master
+import ProductCard from "../components/ProductCard";
 import {Col, Row} from "react-bootstrap";
 import ProductFilter from "./ProductFilter";
 import ShopHeader from "./ShopHeader";
 
 
-<<<<<<< HEAD
-const ProductList = ({onSelect, setCartItems, cartItems}) => {
-    let productList;
-    let totalProducts = 0;
-
-    const {loading, error, data} = useQuery(GET_PRODUCTS);
-
-    if (loading) return 'Loading...';
-    if (error) return `Error! ${error.message}`;
-    //console.log(data);
-
-    const onSelected = selectedProduct => {
-        console.log(selectedProduct); // Printer ut det produkt objektet man trykker på!
-
-        //setSelected([...selected, selectedProduct]);
-        //console.log(selected)
-
-        // Overskriver det som ligger i localStorage, må inkludere prevState
-        localStorage.setItem('cart', JSON.stringify(selectedProduct))
-
-        setCartItems([...cartItems, selectedProduct])
-    };
-
-=======
 const ProductList = ({onSelect, data}) => {
     let productList;
     let totalProducts = 0;
 
->>>>>>> master
     if (data) {
         productList = data.map(product => {
             totalProducts++;
